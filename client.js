650ca479099145599912272e12e93e6bb3d6f280
calamarico <daniel.hernandez.zafra@gmail.com>
2016-03-03 09:54:49 +0100

    ADD host status call

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 var https = require('https'),
   soap = require('soap'),
   extend = require('util')._extend,
+  Q = require('q'),
   config = require('./config.json');
 
 // Avoids DEPTH_ZERO_SELF_SIGNED_CERT error for self-signed certs.
@@ -65,7 +66,7 @@ exports.getComputerGroups = function(socketReq, socketRes) {
 
   soap.createClient(config.soapApiServer, function(err, client) {
       client.hostGroupRetrieveAll(model, function(err, result) {
-          socketRes.json(result.hostGroupRetrieveAllReturn);
+        socketRes.json(result.hostGroupRetrieveAllReturn);
       });
   });
 };
@@ -81,10 +82,27 @@ exports.getComputerHosts = function(socketReq, socketRes) {
 
   soap.createClient(config.soapApiServer, function(err, client) {
       client.hostRetrieveAll(model, function(err, result) {
-          socketRes.json(result.hostRetrieveAllReturn);
+        socketRes.json(result.hostRetrieveAllReturn);
       });
   });
 };
+
+/**
+ * Get Hosts Status using hostGetStatus SOAP method.
+ * @exports _getHostStatus
+ */
+function _getHostStatus(sID, id, deferred) {
+  var model = {
+    sID: sID,
+    id: id
+  };
+
+  soap.createClient(config.soapApiServer, function(err, client) {
+      client.hostGetStatus(model, function(err, result) {
+        deferred.resolve(result);
+      });
+  });
+}
 
 /**
  * Get Computer Hosts Detail using hostDetailRetrieveByName SOAP method.
@@ -95,11 +113,16 @@ exports.getComputerHostsDetail = function(socketReq, socketRes) {
     sID: socketReq.query.sID,
     hostname: socketReq.query.hostname,
     hostDetailLevel: 'HIGH'
-  };
+  },
+    deferred = Q.defer();
 
   soap.createClient(config.soapApiServer, function(err, client) {
       client.hostDetailRetrieveByName(model, function(err, result) {
-          socketRes.json(result.hostDetailRetrieveByNameReturn);
+        _getHostStatus(model.sID, result.hostDetailRetrieveByNameReturn[0].ID, deferred);
+        deferred.promise.then(function(_result) {
+          socketRes.json([extend(
+            result.hostDetailRetrieveByNameReturn[0], _result.hostGetStatusReturn)]);
+        });
       });
   });
 };
