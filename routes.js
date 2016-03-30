7f554e484a665297e781468ee041267a3efc829c
calamarico <daniel.hernandez.zafra@gmail.com>
2016-03-30 21:46:09 +0200

    ADD login route

--- routes.js
+++ routes.js
@@ -15,8 +15,9 @@ exports.initRoutes = function() {
     res.send('TrendMicro Node API Rest');
   });
 
-  router.post('/login', client.login);
-  router.delete('/login', client.logout);
+  router.route('/login')
+    .post(client.login)
+    .delete(client.logout);
   router.get('/computers/groups', client.getComputerGroups);
   router.get('/computers/hosts', client.getComputerHosts);
   router.get('/computers/hosts/detail', client.getComputerHostsDetail);
